7fc8bb857511e24cf9c8da06a8ac9075e4667a43
Wesley Moy <wesley.moy@gmail.com>
2017-03-13 23:53:37 -0700

    Render a basic template

--- furid.go
+++ furid.go
@@ -3,8 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"html/template"
 )
 
+var rootTemplate = template.Must(template.ParseFiles("templates/index.html"))
+
+func root(w http.ResponseWriter, r *http.Request) {
+	rootTemplate.Execute(w, nil)
+}
+
 func main() {
+	http.HandleFunc("/", root)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
